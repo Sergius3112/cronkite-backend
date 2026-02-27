355ade2ada829b4304400760d36326812ccaaf31
Sergius3112 <85290785+Sergius3112@users.noreply.github.com>
2026-02-27 10:54:18 +0000

    Fix Railway build - add Dockerfile

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # FactCheck backend auto-start script
 
-cd /Users/christianstapleton/Downloads/factchecker/backend
+cd /Users/christianstapleton/Downloads/factchecker/cronkite-backend
 source venv/bin/activate
 export GROQ_API_KEY=$GROQ_API_KEY
 uvicorn main:app --host 127.0.0.1 --port 8000
